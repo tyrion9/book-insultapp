0f41bbdc366c3b56fb847b68654c4a7622fba379
Tyrion <hoaipn@gmail.com>
2017-08-11 12:02:47 +0700

    print stacktrace exception

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -9,7 +9,7 @@ public class InsultGenerator {
 	public String generateInsult() {
 		String vowels = "AEIOU";
 		String article = "an";
-		String theInsult = "vai cut: \n";
+		String theInsult = "hello: \n";
 		try {
 			String databaseURL = "jdbc:postgresql://";
 			databaseURL += System.getenv("POSTGRESQL_SERVICE_HOST");
